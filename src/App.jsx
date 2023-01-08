bda4e711f2c0b28d43d438204662e8fbc2d9af57
ertugrulyamaan <97849337+ertugrulyamaan@users.noreply.github.com>
2023-01-08 11:05:28 +0300

    netlify bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import './App.css'
 import { Navbar, Hero, Skills, Projects, About } from './components'
 Navbar
@@ -6,15 +6,15 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <Navbar/>
       <Routes>
-        <Route path="*" index element={<Hero/>}/>
+        <Route path="/" index element={<Hero/>}/>
         <Route path="/skills" element={<Skills/>}/>
         <Route path="/project" element={<Projects/>}/>
         <Route path="/about" element={<About/>}/>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
