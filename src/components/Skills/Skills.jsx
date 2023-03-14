017b5735216714f6cdb83aa56ae324b4d7b4ef78
Ertugrul Yaman <ertugrulyamaan99@gmail.com>
2023-03-14 19:46:34 +0300

    skills added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './skills.css'
 import {AiFillHtml5} from 'react-icons/ai'
+import {SiRedux, SiTailwindcss} from 'react-icons/si'
 import {DiCss3, DiJavascript1, DiReact, DiSass, DiGit} from 'react-icons/di'
 const Skills = () => {
   return (
@@ -28,6 +29,14 @@ const Skills = () => {
       <div className='app__skills-card'>
         <DiGit color='#FC492B' size={150}/>
         <p>Git Version</p>
+      </div>
+      <div className='app__skills-card'>
+        <SiRedux color='#764abc' size={150}/>
+        <p>Redux</p>
+      </div>
+      <div className='app__skills-card'>
+        <SiTailwindcss color='#3490dc' size={150}/>
+        <p>Tailwind CSS</p>
       </div>
     </div>
   )
